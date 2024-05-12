367b4debf23bbff1f3601b1d6d429557ea3c8690
wang huai <heibaimeng@gmail.com>
2024-05-12 11:42:55 +0000

    引入 mongoose ；增加创建用户接口；

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-require('dotenv').config()
+require('dotenv').config();
+const mongoose = require('mongoose')
+
+app.use(express.urlencoded({ extended: true }))
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// 定义 Schema
+const userSchema = new mongoose.Schema({
+  username: String,
+});
+// 根据 Schema 创建模型
+let User = mongoose.model('User', userSchema);
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,6 +21,13 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post('/api/users', (req, res) => {
+  const username = req.body.username
+  const user = new User({
+    username
+  })
+  user.save((err, data) => res.json(data))
+});
 
 
 
